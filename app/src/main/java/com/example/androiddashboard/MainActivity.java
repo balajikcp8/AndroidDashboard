9d895c7c5bf07601ba0de2bfe30fed459a003658
Balaji Thilagar <bath@softwareag.com>
2022-05-10 22:45:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/androiddashboard/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.androiddashboard;
+import java.io.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Initialize Bottom Navigation View.
+        BottomNavigationView navView = findViewById(R.id.bottomNav_view);
+
+        //Pass the ID's of Different destinations
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.first, R.id.second, R.id.third )
+                .build();
+        //Initialize NavController.
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/androiddashboard/SecondFragment.java
@@ -0,0 +1,20 @@
+package com.example.androiddashboard;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import java.io.*;
+
+public class SecondFragment extends Fragment {
+
+    public SecondFragment(){
+        // require a empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+}
